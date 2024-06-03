32811ea5082e4d80f36c61a734daf619b8d167e2
hanriverkitty <danieltls@naver.com>
2024-06-03 15:58:18 +0900

    mysite3 User #finish

--- mysite3/src/main/java/com/poscodx/mysite/controller/MainController.java
+++ mysite3/src/main/java/com/poscodx/mysite/controller/MainController.java
@@ -1,5 +1,10 @@
 package com.poscodx.mysite.controller;
 
+//import java.util.Enumeration;
+//
+//import javax.servlet.ServletContext;
+//import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -7,7 +12,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MainController {
 	
 	@RequestMapping({"/","/main"})
-	public String index() {
-		return "index";
+	public String index(
+			
+			// HttpServletRequest request
+			) {
+//		ServletContext sc = request.getServletContext();
+//		Enumeration<String> names = (Enumeration<String>)sc.getAttributeNames();
+//		while(names.hasMoreElements()){
+//			String name = names.nextElement();
+//			System.out.println(name);
+//		}
+		return "main/index";
 	}
 }
---
+++ mysite3/src/main/java/com/poscodx/mysite/service/GuestbookService.java
@@ -0,0 +1,19 @@
+package com.poscodx.mysite.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GuestbookService {
+	private GuestbookRepository guestbookRepository;
+	public List<GuestbookVo> getContentsList(){
+		return null;
+	}
+	public void deleteContents(Long no, String password) {
+		
+	}
+	public void addContents(GuestbookVo vo) {
+		
+	}
+}
